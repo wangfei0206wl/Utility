2540d95928e888fa22197810ee3cc8437ebd366b
wangfei <wangfei0206wl@163.com>
2022-07-18 15:56:57 +0800

    commit explain GCD Timer vs NSTimer

--- Utility/Utility/Timer/readme.h
+++ Utility/Utility/Timer/readme.h
@@ -24,5 +24,40 @@ NSTimer *timer = [NSTimer timeWithInterval....];
 2)封装NSTimer弱引用target对象，解决循环引用问题（参考DSTimer）
 3)使用NSTimer的block方式
 
+############################################################################################
+gcd timer简介
 
+利用GCD中的Dispatch Source中的一种类型DISPATCH_SOURCE_TYPE_TIMER来实现，监听系统内核对象并处理
+更加的精准，不依赖runloop，不会出现滚动视图滚动时定时器不工作的情况。
+GCD定时器提供了启动、暂停、恢复、取消等功能
+
+流程
+dispatch_source_create创建一个timer类型的源
+dispatch_source_set_timer设置定时器
+dispatch_source_set_event_handler设置定时任务
+dispatch_resume启动定时器
+dispatch_suspend暂停定时器
+dispatch_source_cancel停止定时器
+
+GCD Timer的封装参考GCDTimer
+
+############################################################################################
+GCD Timer 对比 NSTimer
+
+1)NSTimer依赖runloop，所有子线程使用NSTimer需要手动启动runloop，而GCD Timer不依赖于runloop，在子线程中可正常运行.
+2)NSTimer依赖于runloop运行，在某种特定环境下可能会需要runloop模式切换.
+3)NSTimer存在延时的可能性，准确性会有误差，GCD Timer是监听系统内核对象并处理，定时更加精确.
+4)NSTimer容易出现循环引用，GCD Timer相对要好很多.
+
+############################################################################################
+参考
+
+iOS定时器-- NSTimer&GCD定时器
+https://juejin.cn/post/6844904094826512392
+
+iOS gcdTimer VS NSTimer
+https://www.jianshu.com/p/6257361716fd
+
+ios NSTimer和GCDtimer对比
+https://blog.csdn.net/u014600626/article/details/102080330
 
